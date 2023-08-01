c9271fa4a8f2fec566b96a1d602dec522915579e
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2023-08-01 17:28:54 +0900

    First commit

---
+++ netflix-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netflix-clone
+//
+//  Created by 垣本 桃弥 on 2023/08/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
